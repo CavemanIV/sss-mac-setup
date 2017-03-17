c8713700ed562d036cbad9c70865f38021c1012d
CavemanIV <selnteer@gmail.com>
2017-03-17 12:40:30 +0800

    fix typo

--- install.sh
+++ install.sh
@@ -3,9 +3,9 @@
 brew install golang
 go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-local
 
-mkdir -p /usr/local/etc/sslocal/
-cp conf.json /usr/local/etc/sslocal/conf.json
-ln -s $HOME/go/bin/shadowsocks-local /usr/local/bin/shadowsocs-local
+mkdir -p /usr/local/etc/go-ss-local/
+cp conf.json /usr/local/go-ss-local/conf.json
+ln -s $HOME/go/bin/shadowsocks-local /usr/local/bin/shadowsocks-local
 
 cp sslocal.plist $HOME/Library/LaunchAgents/sslocal.plist
 launchctl load $HOME/Library/LaunchAgents/sslocal.plist

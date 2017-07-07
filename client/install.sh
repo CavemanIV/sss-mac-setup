ed6ef97b9a3d22bae7853681d0d63ac79f81c519
CavemanIV <selnteer@gmail.com>
2017-07-07 14:48:41 +0800

    directly render config.json, install brew when neccessary

--- client/install.sh
+++ client/install.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
 
-export http_proxy=http://localhost:8123
-export https_proxy=https://localhost:8123
+source ./install.conf
+SS_LOCAL_ETC=/usr/local/etc/go-ss-local
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+if ! type "brew" > /dev/null; then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
 
 brew install golang
+
+# install shadowsocks
+export http_proxy=http://$PROXY_HOST:$PROXY_HOST
+export https_proxy=https://$PROXY_HOST:$PROXY_PORT
 go get github.com/shadowsocks/shadowsocks-go/cmd/shadowsocks-local
 
-mkdir -p /usr/local/etc/go-ss-local/
-cp conf.json /usr/local/go-ss-local/conf.json
+# setup config
 ln -s $HOME/go/bin/shadowsocks-local /usr/local/bin/shadowsocks-local
+mkdir -p $SS_LOCAL_ETC
+printf '{
+    "local_port":1080,
+    "server_password": [
+        ["%s:%s", "%s", "%s"]
+    ]
+}' "$SS_SERVER_HOST" "$SS_SERVER_PORT" "$SS_SERVER_PWD" $SS_SERVER_AUTH > $SS_LOCAL_ETC/conf.json
 
+# setup initd
 cp sslocal.plist $HOME/Library/LaunchAgents/sslocal.plist
 launchctl load $HOME/Library/LaunchAgents/sslocal.plist
 
-# brew install polipo
-# hp
+# brew install polipo ans setup hp for shell
